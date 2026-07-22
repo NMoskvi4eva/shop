902ed7859415605a786d9504cf39fd3450b88d04
NMoskvi4eva <nadia.moskvi4eva@gmail.com>
2026-07-22 22:37:11 +0300

    Exclude LiqPay webhook from CSRF

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -12,10 +12,15 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware): void {
-        // 🎯 РЕЄСТРУЄМО АДМІНСЬКИЙ MIDDLEWARE ТУТ:
+
+        $middleware->validateCsrfTokens(except: [
+            'liqpay-webhook',
+        ]);
+
         $middleware->alias([
             'admin' => \App\Http\Middleware\AdminMiddleware::class,
         ]);
+
     })
     ->withExceptions(function (Exceptions $exceptions): void {
         $exceptions->shouldRenderJsonWhen(
